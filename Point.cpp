8b9db01af7df73f6a016bd180a829f7df0b34ebd
TheCoolKids2015 <Syd_Lewis@baylor.edu>
2015-04-15 23:34:08 -0500

    Create Point.cpp

---
+++ Point.cpp
@@ -0,0 +1,21 @@
+//Adryanna's really ugly and basic version of a Point
+#include "Point.h"
+
+Point::Point(){
+    x = y = 0;
+}
+
+void Point::setX(int X){
+    x = X;
+}
+
+void Point::setY(int Y){
+    x = Y;
+}
+
+int Point::getX(){
+    return x;
+}
+int Point::getY(){
+    return y;
+}
